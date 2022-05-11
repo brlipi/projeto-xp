73a77eba8704cdc8b14d0c44ad097adb2a41ca37
Breno Lipi <43282619+brlipi@users.noreply.github.com>
2022-05-11 13:42:01 -0300

    Rearraging files locations to accomodate Xunit

---
+++ projeto-xp.Api/projeto-xp.Api/Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+using projeto_xp.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .Enrich.FromLogContext()
+    .CreateLogger();
+
+builder.Logging.ClearProviders();
+builder.Logging.AddSerilog(logger);
+builder.Services.AddControllers();
+/*builder.Services.AddDbContext<UserContext>(opt =>
+    opt.UseInMemoryDatabase("Users"));*/
+builder.Services.AddDbContext<UserContext>(opt =>
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ projeto-xp.Tests/projeto-xp.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace projeto_xp.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
