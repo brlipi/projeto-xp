c267d9047ccdc29ef0e20b8f0edd244a53fc8902
Breno Lipi <43282619+brlipi@users.noreply.github.com>
2022-05-16 13:25:03 -0300

    Rolling interval added

--- projeto-xp.Api/projeto-xp.Api/Extensions/SerilogExtension.cs
+++ projeto-xp.Api/projeto-xp.Api/Extensions/SerilogExtension.cs
@@ -17,7 +17,7 @@ namespace projeto_xp.Api.Extensions
                 .Filter.ByExcluding(Matching.FromSource("Microsoft.AspNetCore.StaticFiles"))
                 .Filter.ByExcluding(z => z.MessageTemplate.Text.Contains("Business error"))
                 .WriteTo.Async(wt => wt.Console(outputTemplate: "[{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} {Level:u3}] {Message:lj} {Properties:j}{NewLine}{Exception}"))
-                .WriteTo.Async(wt => wt.File("logs/webapi-log.log", outputTemplate: "[{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} {Level:u3}] {Message:lj} {Properties:j}{NewLine}{Exception}"))
+                .WriteTo.Async(wt => wt.File("logs/webapi-log.log", rollingInterval: RollingInterval.Day, outputTemplate: "[{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} {Level:u3}] {Message:lj} {Properties:j}{NewLine}{Exception}"))
                 .CreateLogger();
         }
     }
